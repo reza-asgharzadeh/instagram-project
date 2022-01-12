30439ce3db6a19a051f3c2cb63648bd606cfbb98
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-01-12 19:22:20 +0330

    create instagram ui

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,21 +3,47 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+        <div class="col-md-3">
+            <img src="{{asset('/images/reza.jpg')}}" alt="reza" class="rounded-circle border">
         </div>
+
+        <div class="col-md-5">
+            <div class="d-flex justify-content-between align-items-center">
+                <div class="fs-5">reza_asgharzadeh_</div>
+                <button class="btn btn-sm px-4 btn-primary">Follow</button>
+                <button class="btn btn-sm btn-light border">Edit Profile</button>
+            </div>
+
+            <div class="d-flex justify-content-between align-items-center pt-2">
+                <div><strong>3</strong> posts</div>
+                <div><strong>50</strong> followers</div>
+                <div><strong>10</strong> following</div>
+            </div>
+
+            <div class="pt-4"><strong>Reza Asgharzadeh | رضااصغرزاده</strong></div>
+            <div class="text-muted">Software</div>
+            <div>
+                ⭐️اینجا قراره در کنار هم برنامه‌نویسی رو یاد بگیریم 👨🏻‍💻
+                هشت ساعت جستجو به دنبال یک “سمی‌کالن” گم‌شده 🔎
+                ‼️طراحی سایت پذیرفته می‌شود‼️
+            </div>
+            <div><a href="https://wikiato.com" class="text-decoration-none">wikiato.com</a></div>
+        </div>
+    </div>
+    <hr>
+    <div class="row">
+        <div class="d-flex justify-content-center">
+            <div class="mx-4">posts</div>
+            <div class="mx-4">videos</div>
+            <div class="mx-4">saved</div>
+            <div class="mx-4">Tagged</div>
+        </div>
+    </div>
+    <div class="row pt-2">
+        <div class="col-md-4 mb-4"><img src="{{asset('/images/1.png')}}" alt="post" class="img-fluid" style="width: 400px;height:400px"></div>
+        <div class="col-md-4 mb-4"><img src="{{asset('/images/2.png')}}" alt="post" class="img-fluid" style="width: 400px;height:400px"></div>
+        <div class="col-md-4 mb-4"><img src="{{asset('/images/3.png')}}" alt="post" class="img-fluid" style="width: 400px;height:400px"></div>
+        <div class="col-md-4 mb-4"><img src="{{asset('/images/3.png')}}" alt="post" class="img-fluid" style="width: 400px;height:400px"></div>
     </div>
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -19,13 +20,14 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    <img src="{{asset('/images/instagram_logo.png')}}" alt="instagram logo">
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
